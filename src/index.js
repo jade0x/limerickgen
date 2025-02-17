50f332fad12e7d992408901ee263803af5052e4b
jade0x <119149884+jade0x@users.noreply.github.com>
2025-02-17 15:37:25 -0600

    added API and prompt

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-function generateLimerick(event) {
-  event.preventDefault();
+function displayLimerick(response) {
+  console.log("limerick generated");
 
   new Typewriter("#limerick", {
     delay: 50,
     cursor: "",
   })
-    .typeString(
-      "There was an Old Man with a beard, <br>Who said, 'It is just as I feared!<br>Two Owls and a Hen,<br>Four Larks and a Wren, <br>Have all built their nests in my beard!'"
-    )
+    .typeString(response.data.answer)
     .start();
+}
+
+function generateLimerick(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "24f34fb24eae01907fa1460264toc5b2";
+  let context =
+    "You are a poem expert, and you love to write new witty limericks, following the strict AABBA rhyme scheme. You must generate a limerick in basic HTML, but do not write backticks to create a codeblock, and do not provide a title. Make sure to follow the user instructions for the topic of the limerick.";
+  let prompt = `User instructions are: Generate a limerick about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Genearting Limerick");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayLimerick);
 }
 
 let limerickFormElement = document.querySelector("#limerick-generator");
